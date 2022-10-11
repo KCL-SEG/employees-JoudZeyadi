157389341e1a53899a1a005e2624ef8914d6531e
Joud Zeyadi <joud.zeyadi@kcl.ac.uk>
2022-10-12 00:28:20 +0100

    solution 6

--- employee.py
+++ employee.py
@@ -32,7 +32,7 @@ class Employee():
         if self.monthHour == "month" and self.commission == 0:
             string = f"{self.name} works on a monthly salary of {self.get_pay()}.  Their total pay is {self.get_pay()}."
         elif self.monthHour == "month" and self.commission == 1:
-            string = f"{self.name} works on a monthly salary of {self.get_pay()} and receives a commission for {self.cContract} contract(s) at {self.cContractWage}/contract.  Their total pay is {self.get_pay()}."
+            string = f"{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.cContract} contract(s) at {self.cContractWage}/contract.  Their total pay is {self.get_pay()}."
         elif self.monthHour == "month" and self.commission == 2:
             string = f"{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.bCommission}.  Their total pay is {self.get_pay()}."
         elif self.monthHour == "hour" and self.commission == 0:
